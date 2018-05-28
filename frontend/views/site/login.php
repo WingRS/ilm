80255376212a25e556f32e3d426b96c852375231
WingRS <yra100@gmail.com>
2018-05-28 17:09:27 +0300

    fix login page

--- frontend/views/site/login.php
+++ frontend/views/site/login.php
@@ -22,31 +22,3 @@ $this->params['breadcrumbs'][] = $this->title;
 <?= Html::submitButton('Увійти', ['class' => 'btn btn-lg btn-primary btn-block', 'name' => 'login-button']) ?>
 
 <?php ActiveForm::end(); ?>
-<!---<div class="site-login">
-
-
-    <p>Please fill out the following fields to login:</p>
-
-    <div class="row">
-        <div class="col-lg-5">
-
-
-<!--                --><?//= $form->field($model, 'email')->textInput(['autofocus' => true]) ?>
-<!---->
-<!--                --><?//= $form->field($model, 'password')->passwordInput() ?>
-<!---->
-<!--                --><?//= $form->field($model, 'rememberMe')->checkbox() ?>
-
-                <div style="color:#999;margin:1em 0">
-                    If you forgot your password you can <?= Html::a('reset it', ['site/request-password-reset']) ?>.
-                </div>
-
-                <div class="form-group">
-
-                </div>
-
-
-        </div>
-    </div>
-</div>
--->
