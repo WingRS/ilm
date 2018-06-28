9aa7c156da64d17fb85459465edc13e6d1569b8d
Wingrs <yur6996@ukr.net>
2018-06-28 17:15:19 +0300

    ssl

--- environments/prod/common/config/main-local.php
+++ environments/prod/common/config/main-local.php
@@ -10,13 +10,14 @@ return [
         ],
         'mailer' => [
             'class' => 'yii\swiftmailer\Mailer',
+            'useFileTransport' => false,
             'transport' => [
                 'class' => 'Swift_SmtpTransport',
                 'host' => 'smtp.gmail.com',
                 'username' => parse_ini_file("/home/smtp.ini")["email"],
                 'password' => parse_ini_file("/home/smtp.ini")["pass"],
-                'port' => '587',
-                'encryption' => 'tls',
+                'port' => '465',
+                'encryption' => 'ssl',
             ],
             'viewPath' => '@common/mail',
         ],
